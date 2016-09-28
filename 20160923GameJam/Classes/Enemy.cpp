b99ab73f82e02c74c0734d5308faf46296fa9f5a
Aleksi Nissinen <nissinena@elisanet.fi>
2016-09-28 10:42:29 +0900

    Animation updated

--- 20160923GameJam/Classes/Enemy.cpp
+++ 20160923GameJam/Classes/Enemy.cpp
@@ -5,11 +5,54 @@ USING_NS_CC;
 
 bool Enemy::init()
 {
-	Size visibleSize = Director::getInstance()->getVisibleSize();
+	int eType = RandomHelper::random_int(0, 2);
+	//if (eType == CAR)
+	//{
+	//	// Init CAR type enemy
+	//	m_type == CAR;
+	//	m_enemy = Sprite::create("PixelCar.png");
+	//	m_enemy->setScale(1.5);
+	//	m_enemy->setPosition(Vec2((960 / 2) + 850, (640 / 2) + 50));
 
+	//// アクション（スクロール）
+	//auto actionMoveByenemy = MoveBy::create(3.0, Vec2(-1500, 0));
+	//m_enemy->runAction(actionMoveByenemy);
+	//}
+	//else if (eType == BIRD)
+	//{
+		// init BIRD type enemy
+		m_type = BIRD;
+		char str[100];
+		// Load chicken animation
+		SpriteFrameCache::getInstance()->addSpriteFramesWithFile("chicken/flying/flying.plist");
+		Vec2 origin = Director::getInstance()->getVisibleOrigin();
+		Vec2 visibleSize = Director::getInstance()->getVisibleSize();
+		for (int i = 0; i < 3; ++i)
+		{
+			auto spriteCache = SpriteFrameCache::getInstance();
+			sprintf(str, "flying%d.png", i);
+			m_flyingFrames.pushBack(spriteCache->getSpriteFrameByName(str));
+		}
+		auto animation = Animation::createWithSpriteFrames(m_flyingFrames, 0.1f);
+		auto animate = Animate::create(animation);
+		// TODO FIX ANIMATION
+		m_enemy = Sprite::createWithSpriteFrame(m_flyingFrames.front());
+		m_enemy->setScale(0.3);
+		m_enemy->setPosition(Vec2((960 / 2) + 850, (640 / 2) + 70));
+		m_enemy->runAction(RepeatForever::create(animate));
 
-	m_enemy = Sprite::create("enemy.png");
-	m_enemy->setPosition(Vec2((960 / 2) + 850, (640 / 2) + 50));
+		auto actionMoveByenemy = MoveBy::create(5.0, Vec2(-1500, 0));
+		m_enemy->runAction(actionMoveByenemy);
+	//}
+
+	//else if (eType == TEMP)
+	//{
+	//	// Init TEMP type enemy
+	//	m_type == TEMP;
+	//	m_enemy = Sprite::create("enemy.png");
+	//	m_enemy->setPosition(Vec2((960 / 2) + 850, (640 / 2) + 50));
+	//}
+
 	this->addChild(m_enemy);
 
 	//m_bgRight = Sprite::create("bg2Right.png");
@@ -17,10 +60,6 @@ bool Enemy::init()
 	//	(visibleSize.height / 2)));
 	//this->addChild(m_bgRight);
 
-
-	// アクション（スクロール）
-	auto actionMoveByenemy = MoveBy::create(10.0, Vec2(-1500, 0));
-	m_enemy->runAction(actionMoveByenemy);
 	//auto actionMoveBym_bgRight = MoveBy::create(70.0, Vec2(-1500, 0));
 	//m_bgRight->runAction(actionMoveBym_bgRight);
 
--- 20160923GameJam/Classes/Enemy.h
+++ 20160923GameJam/Classes/Enemy.h
@@ -2,11 +2,20 @@
 #define __ENEMY_H__
 
 #include "cocos2d.h"
+#include "cocos2d\cocos\2d\CCSpriteFrame.h"
 
+enum EnemyType
+{
+	CAR = 0,		// Fast mover
+	BIRD = 1,		// Flying
+	TEMP = 2,		// Jumping (TBD)
+};
 class Enemy : public cocos2d::Layer
 {
 private:
 	cocos2d::Sprite* m_enemy;
+	EnemyType m_type;
+	cocos2d::Vector<cocos2d::SpriteFrame*> m_flyingFrames;
 	//cocos2d::Sprite* m_bgRight;
 
 public:
