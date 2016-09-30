b8c88cf878b05b9e2a31a8b259664773b2b520c3
WadaTakumi <tikemushi03@gmail.com>
2016-09-30 13:25:40 +0900

    mask

--- 20160923GameJam/proj.android/jni/Android.mk
+++ 20160923GameJam/proj.android/jni/Android.mk
@@ -12,7 +12,30 @@ LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/BackGround.cpp \
+                   ../../Classes/BackGround2.cpp \
+                   ../../Classes/BackGround3.cpp \
+                   ../../Classes/BackgroundLayer.cpp \
+                   ../../Classes/BaseMask.cpp \
+                   ../../Classes/DebugDrawNode.cpp \
+                   ../../Classes/Enemy.cpp \
+                   ../../Classes/EnemyData.cpp \
+                   ../../Classes/EventListenerGesture.cpp \
+                   ../../Classes/GLES-Render.cpp \
+                   ../../Classes/HelloWorldScene.cpp \
+                   ../../Classes/main.cpp \
+                   ../../Classes/MainScene.cpp \
+                   ../../Classes/MaskOfBullet.cpp \
+                   ../../Classes/MaskOfFly.cpp \
+                   ../../Classes/MaskOfScreenToSpeedUp.cpp \
+                   ../../Classes/MaskOfSpeadUp.cpp \
+                   ../../Classes/MaskPower.cpp \
+                   ../../Classes/MenuScreenScene.cpp \
+                   ../../Classes/Player.cpp \
+                   ../../Classes/Player.cpp            
+                   
+                   
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
